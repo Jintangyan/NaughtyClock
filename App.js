783b880286bc1e979f6674d6111386d31cf99fe8
Jintang Yan <9180@ait.nsw.edu.au>
2023-09-22 16:20:43 +1000

    homePage

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// App.js
 
-export default function App() {
+import React, { useState } from 'react';
+import { View, Text, Button, FlatList, StyleSheet } from 'react-native';
+
+const App = () => {
+  const [alarms, setAlarms] = useState([
+    { id: '1', time: '07:00 AM' },
+    { id: '2', time: '08:30 AM' },
+  ]);
+
+  const addAlarm = () => {
+    // Logic to add a new alarm
+    // For simplicity, we'll add a dummy alarm here
+    const newAlarm = { id: Date.now().toString(), time: '09:00 AM' };
+    setAlarms((prevAlarms) => [...prevAlarms, newAlarm]);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>This is our app</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        data={alarms}
+        renderItem={({ item }) => <Text style={styles.alarmTime}>{item.time}</Text>}
+        keyExtractor={(item) => item.id}
+      />
+      <Button title="Add Alarm" onPress={addAlarm} />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#f5f5f5',
+  },
+  alarmTime: {
+    fontSize: 24,
+    marginVertical: 10,
   },
 });
+
+export default App;
